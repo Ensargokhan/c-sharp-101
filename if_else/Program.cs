1cd33c04fa3e78dc3742039e126c03b62aff1db8
ensar_gokhan <ensargkhn1@gmailcom>
2025-04-21 23:14:45 +0300

    değişiklik yaoıldı

---
+++ if_else/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int time = DateTime.Now.Hour;
+
+
+if(time >= 6 && time <=11)
+{
+    Console.WriteLine("günaydın");
+}
+else if(time <= 18)
+{
+    Console.WriteLine("iyi günler");
+}
+else
+{
+    Console.WriteLine("iyi geceler");
+}
+
+string sonuc = time <= 18 ? "iyi günler" : "iyi geceler";
+Console.WriteLine(sonuc);
+int sicaklik = 15;
+
+
+string durum = (sicaklik > 25) ? "Sıcak" : (sicaklik > 15) ? "Ilık" : "Soğuk";
+Console.WriteLine(durum);
