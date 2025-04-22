c0b1ebc0e3b100f004aeb25649e53d55d13b2720
ensar_gokhan <ensargkhn1@gmailcom>
2025-04-22 20:04:41 +0300

    metodlar konusu çalışması

---
+++ metods/Program.cs
@@ -0,0 +1,19 @@
+﻿int sayi1 = 2, sayi2 = 3;
+
+int sonuc = Topla(sayi1,sayi2);
+Console.WriteLine(sonuc);
+Metods ornek = new Metods();
+ornek.PrintScreen(Convert.ToString(sonuc));
+
+int Topla(int sayi1, int sayi2)
+{
+    return (sayi1 + sayi2);
+}
+
+class Metods
+{
+    public void PrintScreen(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+}
