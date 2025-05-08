80e8ed0e404a19882397c4843ec92b7e2775949f
ensar_gokhan <ensargkhn1@gmailcom>
2025-05-08 18:47:35 +0300

    ödevler 1 de 3. algoritma çözüldü

---
+++ ödevler/algoritma3/Program.cs
@@ -0,0 +1,21 @@
+﻿int n;
+
+Console.Write("lütfen bir sayı giriniz: ");
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("hatalı sayı girdiniz: ");
+}
+
+string[] words = new string[n];
+
+for (int i = 0; i < n; i++)
+{
+    string word;
+    Console.Write($"{i + 1}. kelimeyi giriniz: ");
+    word = Console.ReadLine();
+    words[i] = word;
+}
+for (int i = words.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(words[i]);
+}
