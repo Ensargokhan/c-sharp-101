af3b069138f0c1f23ab14fc809b34042daeec84d
ensar_gokhan <ensargkhn1@gmailcom>
2025-05-08 18:27:34 +0300

    ödevler 1 de 2. algoritma çözüldü

---
+++ ödevler/algoritma2/Program.cs
@@ -0,0 +1,34 @@
+﻿int n, m;
+
+Console.Write("1. sayıyı giriniz: ");
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("hatalı sayı girdiniz");
+}
+
+Console.Write("2. sayıyı giriniz: ");
+while (!int.TryParse(Console.ReadLine(), out m) || m <= 0)
+{
+    Console.WriteLine("hatalı sayı girdiniz");
+}
+
+int[] sayilar = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int sayi;
+    Console.Write($"{i + 1}. sayıyı giriniz:");
+    while (!int.TryParse(Console.ReadLine(), out sayi) || sayi <= 0)
+    {
+        Console.WriteLine("hatalı sayı girdiniz");
+    }
+    sayilar[i] = sayi;
+}
+
+foreach (int sayi in sayilar)
+{
+    if (sayi % m == 0)
+    {
+        Console.WriteLine(sayi);
+    }
+}
