0ba58eb1136bfb71f6ae07acbc5c0de41791445c
ensar_gokhan <ensargkhn1@gmailcom>
2025-05-08 18:14:07 +0300

    ödevler 1 de 1. algoritma çözümü

---
+++ ödevler/algoritma1/Program.cs
@@ -0,0 +1,30 @@
+﻿int n;
+
+Console.Write("lütfen bir sayı giriniz:");
+
+while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+{
+    Console.WriteLine("hatalı sayı girdiniz");
+}
+
+int[] sayilar = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int sayi;
+    Console.Write($"{i + 1}. sayıyı giriniz: ");
+    while (!int.TryParse(Console.ReadLine(), out sayi) || n <= 0)
+    {
+        Console.WriteLine("hatalı sayı girdiniz");
+    }
+    sayilar[i] = sayi;
+}
+
+Console.WriteLine("\nÇift sayılar");
+foreach (int sayi in sayilar)
+{
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine(sayi);
+    }
+}
