7cc8b5563db5f25800ab15c96600d1aa24b186e7
ensar_gokhan <ensargkhn1@gmailcom>
2025-09-21 18:06:30 +0300

    enum konusu

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.pazar);
+            Console.WriteLine((int)Gunler.cumartesi);
+        }
+    }
+    enum Gunler
+    {
+        pazartesi,
+        sali,
+        carsamba,
+        persembe,
+        cuma,
+        cumartesi,
+        pazar
+    }
+}
